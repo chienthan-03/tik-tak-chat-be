e68e14f756c8a7f113a93894b6c775e0a6a716f3
LuanThanh10d <khucthanhluan1002@gmail.com>
2023-03-23 01:36:38 +0700

    three commit

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.routes");
 const chatRoutes = require("./routes/chat.routes");
 const messageRoutes = require("./routes/message.routes");
+const cors = require("cors");
+
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT;
 // const path = require("path");
 
 app.use(express.json()); //to accept JSON data
+app.use(cors());
 
 connectDB();
 
