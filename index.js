0908cd7e764bf6970264ebe70676ee91f342fbae
LuanThanh10d <khucthanhluan1002@gmail.com>
2023-03-28 23:25:34 +0700

    four commit

--- index.js
+++ index.js
@@ -28,13 +28,34 @@ app.use(errorHandler);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3001",
-    // credentials: true,
+    origin: "http://localhost:3000",
   },
 });
 
 io.on("connection", (socket) => {
   console.log("connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id); //kết nối 1 phòng riêng
+    console.log(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined room: ", room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+    if (!chat.users) return console.log("chat.users not defined");
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageRecieved); //gửi tin nhắn mới đến các user có phòng là idUser được tạo từ trước
+    });
+  });
 
   socket.on("disconnect", (reason) => {
     console.log("Disconnected due to:", reason);
