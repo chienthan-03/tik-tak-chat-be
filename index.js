adeac6510c1847c18e8268a012e4ccf220056e17
Thanh Luan <luan.khucthanh@euroland.com>
2025-05-19 15:42:51 +0700

    Enhance typing event handling: added logging for typing and stop typing events in specific rooms

--- index.js
+++ index.js
@@ -87,8 +87,15 @@ io.on("connection", (socket) => {
     console.log("User joined room: ", room);
   });
 
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on("typing", (room) => {
+    console.log("Server received typing event for room:", room);
+    socket.to(room).emit("typing");
+  });
+
+  socket.on("stop typing", (room) => {
+    console.log("Server received stop typing event for room:", room);
+    socket.to(room).emit("stop typing");
+  });
 
   //call video
   socket.on("initiateCall", (data) => {
